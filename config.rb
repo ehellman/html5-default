3a83643e82cddfa84bad584a39bf667887550b01
Erik Hellman <iam@erikhellman.com>
2012-12-11 12:52:28 +0100

    Add retina extension

--- config.rb
+++ config.rb
@@ -1,3 +1,9 @@
+# import sass extensions
+retina_ext = File.join(File.dirname(__FILE__), 'dev/extensions/retina-images')
+require File.join(retina_ext, 'lib', 'sass_extensions.rb')
+add_import_path File.join(retina_ext, 'stylesheets')
+
+# compass
 http_path        = "/"
 sass_dir         = "dev/sass"
 css_dir          = "lib/css"
